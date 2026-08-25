8ce7c5c9b8fd9a8e8e73a8f76a75e1009c5b0b8b
Mayank <yadavmayank1804@gmail.com>
2026-08-25 23:29:36 +0530

    Update day 3

--- Day 3/loops.java
+++ Day 3/loops.java
@@ -1,12 +1,88 @@
-import java.util.Scanner;
+
+
 
 public class loops{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("How many time you want the loop to run - ");
-        int a = sc.nextInt();
-        for (int counter = 0; counter<a; counter+=1 ){
-            System.out.println("Hey, I am Max " + counter);
-        }
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("How many time you want the loop to run - ");
+        // int a = sc.nextInt();
+        // for (int counter = 0; counter<a; counter+=1 ){
+        //     System.out.println("Hey, I am Max " + counter);
+        // }
+        //Print the sum of first n natural number 
+        // n = 5
+        // int sum = 0;
+        // for(int i = 1; i < 6; i++){
+        //     sum+=i;
+        // }
+
+        // System.out.println(sum);
+        // int i = 0;
+        // int sum = 0;
+        // while(i<6){
+        //     sum+=i;
+        //     i++;
+        // }
+        // System.out.println(sum);
+
+        // for(; ;) {
+        //     System.out.println("Apna College");
+        // }
+//---------------------------------------------------------------
+        //Patterns
+
+        //rectangle 
+        // int n = 4;
+        // int m = 5;
+        // for (int i=1; i<=n;i++){
+        //     for(int j=1; j<=m; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+//--------------------------------------------------------------
+        //Print this
+        // *****
+        // *   *
+        // *   *
+        // *****
+//---------------------------------------------------------------
+        // int n=4;
+        // int m=5;
+        // for(int i=1; i<=n; i++){
+        //     for( int j=1; j<=m; j++){
+        //         if( i ==1 || j ==1 || i==n || j==m){
+        //             System.out.print("*");
+        //         }else{
+        //             System.out.print(" ");
+        //         }
+        //     }System.out.println();
+        // }
+// -------------------------------------------------------------
+        // Print this 
+        // *
+        // **
+        // ***
+        // ****
+        // int n = 4;
+        // for(int i=1; i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+//---------------------------------------------------------
+        // print this
+        // ****
+        // ***
+        // **
+        // *
+        // int n=4;
+        // for(int i=n; i>=1;i--){
+        //     for(int j=i;j>=1;j--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
     }
 }
