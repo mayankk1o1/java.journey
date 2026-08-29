fdd536bd2a35f9860e5204867d1a324a1ce1ab2a
Mayank <yadavmayank1804@gmail.com>
2026-08-29 23:08:50 +0530

    Update

---
+++ Day 4/MyClass.java
@@ -0,0 +1,25 @@
+class computer
+{   
+    public void playMusic()
+    {
+        System.out.println("Music Playing....");
+    }
+    public int displaynumber()
+    {
+        // num+=1;
+        return 56;
+    }
+
+}
+
+public class MyClass{
+        public static void main(String[] a) {
+        
+            computer comp= new computer();
+            comp.playMusic();
+            int num2 = comp.displaynumber();
+            System.out.println(num2);
+
+
+    }
+}
